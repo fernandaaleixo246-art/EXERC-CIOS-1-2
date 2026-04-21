d8b5d2be9c968c1448832b991c6b3df005daeb7a
Jennifer <Jennifer@JONATHA-DELL>
2026-04-21 18:01:09 -0300

    Adicionar arquivos de projeto.

---
+++ Retangulo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a base:");
+            double baseRet = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a altura");
+            double altura = double.Parse(Console.ReadLine());
+
+
+
+            double area = baseRet * altura;
+
+            Console.WriteLine("area do retângulo é:" + area);
+            Console.ReadKey();
+
+
+        }
+    }
+}
