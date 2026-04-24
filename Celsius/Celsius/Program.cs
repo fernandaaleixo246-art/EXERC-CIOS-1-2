1fff7a0ee42f75e760d602027c1314b6d3e056bf
fernandaaleixo246-art <fernandaaleixo246@gmail.com>
2026-04-23 22:39:40 -0300

    Add files via upload

---
+++ Celsius/Celsius/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celsius
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius, fahrenheit;
+
+            Console.WriteLine("Digite a temperatura em celsius:");
+            celsius = double.Parse(Console.ReadLine());
+
+            fahrenheit = (9.0 / 5.0) * celsius +  32;
+
+            Console.WriteLine("Temperaturaem Fahrenheit:" + fahrenheit);
+
+
+        
+        }
+    }
+}
