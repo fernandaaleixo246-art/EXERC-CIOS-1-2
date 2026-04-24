49522fe9285aa34ded8d1dd8e7eba92b1956763c
fernandaaleixo246-art <fernandaaleixo246@gmail.com>
2026-04-23 22:53:35 -0300

    Add files via upload

---
+++ Circulo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Circulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double diametro, raio, area;
+
+            Console.Write("Digite o diâmetro:");
+            diametro = double.Parse(Console.ReadLine());
+
+            raio = diametro / 2;
+            area = raio * 2;
+
+            Console.Write("Área do circulo:" +  area);  
+
+
+
+        }
+    }
+}
