491b2c35d649c2e0b5f7da77fa7fc70b79a5b44b
fernandaaleixo246-art <fernandaaleixo246@gmail.com>
2026-04-23 21:17:51 -0300

    Add files via upload

---
+++ Quadrado/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quadrado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double aresta, area; 
+            Console.WriteLine("Digite o valor da aresta do quadrado"); 
+            aresta = double.Parse(Console.ReadLine());
+
+            area = aresta * aresta;
+
+            Console.WriteLine("Area = " + area); 
+
+
+             
+           
+
+
+
+        }
+    }
+}
