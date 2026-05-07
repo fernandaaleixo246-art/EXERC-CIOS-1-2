84b250813f01549fece5838f40db55e1463631ee
fernandaaleixo246-art <fernandaaleixo246@gmail.com>
2026-05-07 16:31:21 -0300

    Add files via upload

---
+++ Reais/Reais/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reais
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            double  cotacaoDolar, quantidadeDolar, valorReal;
+
+            Console.Write("Digite a cotação do dólar: ");
+            cotacaoDolar = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a quantidade de dólares: ");
+            quantidadeDolar = Convert.ToDouble(Console.ReadLine());
+
+            valorReal = cotacaoDolar * quantidadeDolar;
+
+            Console.WriteLine("Valor em reais: R$ + valorEmReais ");
+
+            Console.ReadKey();
+        }
+    }
+}
