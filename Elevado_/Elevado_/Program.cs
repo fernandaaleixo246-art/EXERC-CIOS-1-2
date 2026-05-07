c004e98febd5d0964572a7072fcbe4a8ab19442b
fernandaaleixo246-art <fernandaaleixo246@gmail.com>
2026-05-07 16:37:58 -0300

    Add files via upload

---
+++ Elevado_/Elevado_/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elevado_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, resultado;
+
+            Console.WriteLine("Digite o valor de x:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor de y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            resultado = Math.Pow(x, y);
+
+            Console.WriteLine("Resultado de x elevado a y " + resultado);
+
+            Console.ReadKey();
+        }
+    }
+}
